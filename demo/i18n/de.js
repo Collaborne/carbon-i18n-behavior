f72c4eb2353646a6105c3953d7a72b09d39be372
Ronny Roeller <ronny@collaborne.com>
2016-01-26 11:42:31 +0100

    Support translations for multiple element

--- demo/i18n/de.js
+++ demo/i18n/de.js
@@ -1,5 +1,5 @@
 !function() {
-    Polymer.CarbonI18nBehaviorLocales.add('de', {
+    Polymer.CarbonI18nBehaviorLocales.add('simple-i18n', 'de', {
         hello: 'Hallo Welt',
         members: '{0} Mitglieder',
         intro: 'Mein Name ist {0} und ich bin {1} Jahre alt.'
--- demo/i18n/en.js
+++ demo/i18n/en.js
@@ -1,5 +1,5 @@
 !function() {
-    Polymer.CarbonI18nBehaviorLocales.add('en', {
+    Polymer.CarbonI18nBehaviorLocales.add('simple-i18n', 'en', {
         hello: 'Hello World',
         members: '{0} members',
         intro: 'My name is {0} and I am {1} years old.'
